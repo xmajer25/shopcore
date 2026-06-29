b066436c998a52fae29367bab56f02b93fa74f47
Jakub Majer <567782@mail.muni.cz>
2026-06-30 00:54:31 +0200

    Initial microservice project struct

---
+++ api-gateway/src/test/java/com/xmajer/shopcore/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmajer.shopcore.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ product-service/src/test/java/com/xmajer/shopcore/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmajer.shopcore.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
